3a493201de2ab5f34b5d1cdf6e4dc2cfe6987032
GovindRakholiyaTMI <116937595+GovindRakholiyaTMI@users.noreply.github.com>
2023-02-16 09:30:21 +0530

    textPlacement

--- src/components/TechaeonCoin/index.tsx
+++ src/components/TechaeonCoin/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Layer, Path, Stage } from "react-konva";
+import { Layer, Path, Stage, Text, TextPath } from "react-konva";
 import tinycolor, { ColorFormats } from "tinycolor2";
 import { generatePalette } from "../../utils";
+import { useTextWidth } from '@imagemarker/use-text-width';
 
 export enum Shapes {
   square = "M 240 120 Q 240 0 120 0 Q 0 0 0 120 Q 0 240 120 240 Q 240 240 240 120 Z",
@@ -19,6 +20,16 @@ const BASE_SIZE = 240;
 const TechaeonCoin = ({ scale = 1, shape, color }: Props) => {
   const palette = generatePalette(color);
   const size = 260 * scale;
+  const startingXPointForText = size - (size * 0.9)
+  const endingXPointForText = size - (size * 0.1)
+  const startingYPointForText = size - (size * 0.9)
+  const endingYPointForText = size - (size * 0.1)
+
+  const width = useTextWidth({ text: 'AEONPASS TECHAEON', font: '12px Times' });
+  console.log(width)
+
+  const topTextPath = "M " + startingXPointForText + "," + startingYPointForText + " Q " + ((startingXPointForText+endingXPointForText)/2) + "," + (startingYPointForText - 15) + " " + endingXPointForText + "," + startingYPointForText
+  
 
   // const gradient1 = `linear-gradient(`
 
@@ -77,13 +88,20 @@ const TechaeonCoin = ({ scale = 1, shape, color }: Props) => {
               1,
               palette[100],
             ]}
+            
           />
+          <TextPath fill={'#333'} fontSize={12} fontFamily={'Times'} text={"AEONPASS TECHAEON"} data={'M 70,50  Q 130,18 190,50'} align={'center'} ></TextPath>
+          <TextPath fill={'#333'} fontSize={12} fontFamily={'Arial-Bold'} text={"AEONPASS TECHAEON"} data={'M 200,60  Q 240,130 200,200'} align={'center'}></TextPath>
+          <TextPath fill={'#333'} fontSize={12} fontFamily={'Arial'} text={"AEONPASS TECHAEON"} data={'M 200,200  Q 130,240 60,200'} align={'center'}></TextPath>
+          <TextPath fill={'#333'} fontSize={12} fontFamily={'Arial-Bold'} text={"AEONPASS TECHAEON"} data={'M 60,200  Q 20,130 60,60'} align={'center'}></TextPath>
+          {/* <TextPath x={0} y={0} fill={'#333'} fontSize={16} fontFamily={'Arial'} text={"All the world's a stage, and all the men and women merely players."} data={'M 240 120 Q 240 0 120 0 Q 0 0 0 120 Q 0 240 120 240 Q 240 240 240 120 Z'}></TextPath> */}
           <Path
             x={BASE_SIZE * 0.15 + STARTING_POINT}
             y={BASE_SIZE * 0.15 + STARTING_POINT}
             data={shape}
             scale={{ x: 0.7, y: 0.7 }}
             fill="red"
+
           />
         </Layer>
       </Stage>
