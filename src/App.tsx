d9ade21203ee44da06d573e28efe85bd63710f9c
GovindRakholiyaTMI <116937595+GovindRakholiyaTMI@users.noreply.github.com>
2023-02-20 15:23:47 +0530

    working on download image

--- src/App.tsx
+++ src/App.tsx
@@ -11,31 +11,31 @@ function App() {
       <SiteHeader />
       {/* <Coin /> */}
       <div className="w-full flex align-middle justify-center">
-      <div className="w-80 flex align-middle justify-center self-center">
-      <motion.div
-        animate={{
-          scale: [1, 1.5, 2, 1.5, 1],
-          rotate: [0, 0, 360, 360, 0],
-          // rotateZ:[0, 0, 360, 360, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+        <div className="w-80 flex align-middle justify-center self-center">
+          <motion.div
+            animate={{
+              scale: [1, 1.5, 2, 1.5, 1],
+              rotate: [0, 0, 360, 360, 0],
+              // rotateZ:[0, 0, 360, 360, 0],
+              borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+
+            }}
+            transition={{ duration: 2.0 }}
+          >
+
+            <TechaeonCoin
+              shape={Shapes.square}
+              color={{ r: 255, g: 194, b: 38 }}
+              scale={1}
+              branding={"AEONPASS TECHAEON  AEONPASS TECHAEON  AEONPASS TECHAEON  AEONPASS TECHAEON"}
+            />
+
+
+          </motion.div>
+        </div>
+      </div>
+
           
-        }}
-        transition={{ duration: 2.0 }}
-      >
-        
-          <TechaeonCoin
-            shape={Shapes.square}
-            color={{ r: 255, g: 194, b: 38 }}
-            scale={1}
-            branding={"AEONPASS TECHAEON  AEONPASS TECHAEON  AEONPASS TECHAEON  AEONPASS TECHAEON"}
-          />
-        
-
-      </motion.div>
-      </div>
-      </div>
-
-
 
 
 
