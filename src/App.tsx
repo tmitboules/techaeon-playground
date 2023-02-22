5b5e6868f19c60213a42eed59eff1443a29a3faa
tmitboules <88787579+tmitboules@users.noreply.github.com>
2023-02-22 11:30:02 -0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
           </div>
         </section>
 
-        <div className="w-full flex-1 flex flex-col justify-center items-center">
+        <div className="w-full flex-1 flex flex-col justify-center items-center gap-4">
           <TechaeonCoin
             shape={shape}
             color={color}
@@ -52,6 +52,12 @@ function App() {
             className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Animate
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Download
           </button>
         </div>
       </div>
