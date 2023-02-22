c423f182d4f5ce1a0f540c88e245f0817dec4509
tmitboules <88787579+tmitboules@users.noreply.github.com>
2023-02-22 11:28:21 -0800

    animate button

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
           </div>
         </section>
 
-        <div className="w-full flex-1 flex align-middle justify-center">
+        <div className="w-full flex-1 flex flex-col justify-center items-center">
           <TechaeonCoin
             shape={shape}
             color={color}
@@ -47,6 +47,12 @@ function App() {
             imageUrl="./lion-logo.png"
             // imageUrl="https://konvajs.org/assets/lion.png"
           />
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Animate
+          </button>
         </div>
       </div>
 
