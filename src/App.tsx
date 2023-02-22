28f8de47d51fad95035d0c0f53b8ca05eb70cecc
tmitboules <88787579+tmitboules@users.noreply.github.com>
2023-02-22 10:42:58 -0800

    small updates to pickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import SiteHeader from "./components/SiteHeader";
-import TechaeonCoin, { Shapes } from "./components/TechaeonCoin";
+import TechaeonCoin from "./components/TechaeonCoin";
+import useTechaeonParams from "./hooks/useTechaeonParams";
 
 function App() {
+  const { shape } = useTechaeonParams();
+
   return (
     <main>
       <SiteHeader />
-      <div className="w-full flex align-middle justify-center">
-        <TechaeonCoin
-          shape={Shapes.square}
-          color={{ r: 63, g: 63, b: 63 }}
-          scale={1}
-          branding={"AEONPASS TECHAEON"}
-          imageUrl="./lion-logo.png"
-          // imageUrl="https://konvajs.org/assets/lion.png"
-        />
+      <div className="flex">
+        <div className="flex-1 h-10 bg-blue-400"></div>
+        <div className="w-full flex-1 flex align-middle justify-center">
+          <TechaeonCoin
+            shape={shape}
+            color={{ r: 63, g: 63, b: 63 }}
+            scale={1}
+            branding={"AEONPASS TECHAEON"}
+            imageUrl="./lion-logo.png"
+            // imageUrl="https://konvajs.org/assets/lion.png"
+          />
+        </div>
       </div>
 
-      <div className="w-full flex align-middle justify-center">
+      {/* <div className="w-full flex align-middle justify-center">
         <TechaeonCoin
           shape={Shapes.diamond}
           color={{ r: 180, g: 120, b: 1 }}
@@ -34,7 +40,7 @@ function App() {
           branding={"AEONPASS TECHAEON"}
           imageUrl="./elegant_gold_lion.png"
         />
-      </div>
+      </div> */}
     </main>
   );
 }
