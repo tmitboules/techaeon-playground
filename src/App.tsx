e824fdfda6ce35f16f744ad3b7643ef0b1a88ca6
tmitboules <88787579+tmitboules@users.noreply.github.com>
2023-05-12 13:21:08 -0700

    clip func working

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-
-
-import { Link, Backdrop, CircularProgress } from '@mui/material';
-import Grid2 from '@mui/material/Unstable_Grid2';
+import { Link, Backdrop, CircularProgress } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
 import { ImagesResponseDataInner } from "openai";
-import { useEffect } from 'react';
-
+import { useEffect } from "react";
 
 import ColorButton from "./components/ColorButtons";
 import ShapeButton from "./components/ShapeButtons";
 import SiteHeader from "./components/SiteHeader";
 import TechaeonCoin from "./components/TechaeonCoin";
-import useImageSearchHook from './hooks/useImageSearchHook';
+import TechaeonCoin3 from "./components/TechaeonCoin3";
+import useImageSearchHook from "./hooks/useImageSearchHook";
 import useTechaeonParams, {
   ShapeKeys,
   techaeonColors,
@@ -18,18 +16,49 @@ import useTechaeonParams, {
 } from "./hooks/useTechaeonParams";
 
 function App() {
-  const { shape, setShape, color, setColor, img, setImg, setShowImageSearchOption, showImageSearchOption } = useTechaeonParams();
-  const { generateImageOnString, searchedImages, openFileSelector, selectedImage, setSelectedImage, searchedPrompt, setSearchedPrompt, imageSearchLoading, setSearchedImages } = useImageSearchHook()
+  const {
+    shape,
+    setShape,
+    color,
+    setColor,
+    img,
+    setImg,
+    setShowImageSearchOption,
+    showImageSearchOption,
+  } = useTechaeonParams();
+  const {
+    generateImageOnString,
+    searchedImages,
+    openFileSelector,
+    selectedImage,
+    setSelectedImage,
+    searchedPrompt,
+    setSearchedPrompt,
+    imageSearchLoading,
+    setSearchedImages,
+  } = useImageSearchHook();
 
   useEffect(() => {
-    setImg(selectedImage)
-  }, [selectedImage])
+    setImg(selectedImage);
+  }, [selectedImage]);
 
   const ImageItem = (item: ImagesResponseDataInner) => {
-    return <
-      Link sx={{ ":hover": { cursor: "pointer" } }}> <img onClick={() => { setSelectedImage(item.url) }} style={{ height: 180, width: 180 }} className="result-images" src={item.url} alt="Generated Image" /></Link>
-  }
-  
+    return (
+      <Link sx={{ ":hover": { cursor: "pointer" } }}>
+        {" "}
+        <img
+          onClick={() => {
+            setSelectedImage(item.url);
+          }}
+          style={{ height: 180, width: 180 }}
+          className="result-images"
+          src={item.url}
+          alt="Generated Image"
+        />
+      </Link>
+    );
+  };
+
   return (
     <main>
       <SiteHeader />
@@ -59,16 +88,15 @@ function App() {
               />
             ))}
           </div>
-       
+
           <div className="flex gap-2 flex-1 mt-10">
             <button
               onClick={() => {
-                openFileSelector()
-                setShowImageSearchOption(false)
-                setSearchedPrompt('')
-                setSearchedImages([])
-              }
-              }
+                openFileSelector();
+                setShowImageSearchOption(false);
+                setSearchedPrompt("");
+                setSearchedImages([]);
+              }}
               type="button"
               className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
@@ -77,7 +105,9 @@ function App() {
 
             <button
               onClick={() => {
-                setShowImageSearchOption((showImageSearchOption) => !showImageSearchOption)
+                setShowImageSearchOption(
+                  (showImageSearchOption) => !showImageSearchOption
+                );
               }}
               type="button"
               className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -86,15 +116,22 @@ function App() {
             </button>
           </div>
 
-          {showImageSearchOption ?
-            <div style={{ marginBottom: 10}}  >
+          {showImageSearchOption ? (
+            <div style={{ marginBottom: 10 }}>
               <div className="flex gap-2 flex-1 mt-10">
-                <input value={searchedPrompt} onChange={e => setSearchedPrompt(e.target.value)} type="text" id="search_image_input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Image" required>
-                </input>
+                <input
+                  value={searchedPrompt}
+                  onChange={(e) => setSearchedPrompt(e.target.value)}
+                  type="text"
+                  id="search_image_input"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search Image"
+                  required
+                ></input>
 
                 <button
                   onClick={() => {
-                    generateImageOnString(searchedPrompt)
+                    generateImageOnString(searchedPrompt);
                   }}
                   type="button"
                   className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -103,69 +140,35 @@ function App() {
                 </button>
               </div>
 
-              {imageSearchLoading ?
+              {imageSearchLoading ? (
                 <Backdrop open>
                   <CircularProgress color="inherit" />
                 </Backdrop>
-                : <></>}
+              ) : (
+                <></>
+              )}
 
-              {searchedImages.length > 0 ?
+              {searchedImages.length > 0 ? (
                 <div style={{ margin: 10 }}>
                   <Grid2 container gap={2} columns={3}>
                     {searchedImages.map((item) => {
-                      return ImageItem(item)
+                      return ImageItem(item);
                     })}
                   </Grid2>
-                </div> : <></>}
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
-            : <></>}
+          ) : (
+            <></>
+          )}
         </section>
 
         <div className="w-full flex-1 flex flex-col items-center gap-4">
-          <TechaeonCoin
-            shape={shape}
-            color={color}
-            scale={1}
-            isCustomImage={img !== '' && img !== undefined}
-            branding={"AEONPASS TECHAEON"}
-            imageUrl={img ? img : "./lion-logo.png"}
-          // imageUrl="https://konvajs.org/assets/lion.png"
-          />
-          {/* <>{View}</> */}
-          {/* <button
-            type="button"
-            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Animate
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Download
-          </button> */}
+          <TechaeonCoin3 shape={shape} color={color} scale={2} />
         </div>
       </div>
-
-      {/* <div className="w-full flex align-middle justify-center">
-        <TechaeonCoin
-          shape={Shapes.diamond}
-          color={{ r: 180, g: 120, b: 1 }}
-          scale={1}
-          branding={"AEONPASS TECHAEON"}
-          imageUrl="./lion-logo-safari.png"
-        />
-      </div>
-
-      <div className="w-full flex align-middle justify-center">
-        <TechaeonCoin
-          shape={Shapes.hexagon}
-          color={{ r: 150, g: 100, b: 20 }}
-          scale={1}
-          branding={"AEONPASS TECHAEON"}
-          imageUrl="./elegant_gold_lion.png"
-        />
-      </div> */}
     </main>
   );
 }
---
+++ src/components/TechaeonCoin3/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Layer, Path, Rect, Shape, Stage } from "react-konva";
+import { ColorFormats } from "tinycolor2";
+import { generatePalette, TechaeonPalette } from "../../utils";
+
+interface Props {
+  scale: number;
+  shape: string;
+  color: ColorFormats.RGB;
+}
+
+const TechaeonCoin3 = ({ scale, shape, color }: Props) => {
+  const palette = generatePalette(color);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const grd = ctx?.createLinearGradient(0, 50, 90, 100);
+  grd?.addColorStop(0.0, palette[100]);
+  grd?.addColorStop(1 / 3, palette[500]);
+  grd?.addColorStop(1 / 2, palette[200]);
+  grd?.addColorStop(1, palette[800]);
+
+  const size = 240;
+  const bezel = size * 0.07;
+
+  const fullSize = (bezel + size) * scale;
+
+  return (
+    <Stage width={fullSize} height={fullSize} scale={{ x: scale, y: scale }}>
+      <Layer
+        clipFunc={(ctx) => {
+          const path = new Path2D(shape);
+          ctx.rect(fullSize, 0, -fullSize, fullSize);
+          ctx._context.clip(path);
+        }}
+        x={bezel / 2}
+        y={bezel / 2}
+      >
+        <Path data={shape} fill="blue" />
+        {/* Imagine this is an image  */}
+        <Rect width={fullSize} height={fullSize} fill="red" />
+      </Layer>
+      {/* bezel */}
+      <Layer style={{ overflow: "hidden" }} x={bezel / 2} y={bezel / 2}>
+        <Path data={shape} strokeWidth={bezel} stroke={grd} />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default TechaeonCoin3;
